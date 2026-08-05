6dc08e7bbf067a45b1c5c1c6a95b5f903315721f
kinnaamf <dimoje.1337@gmail.com>
2026-08-05 23:35:30 +0300

    feat: add app routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\GenerateController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('app');
+    return Inertia::render('Generator');
 });
+
+Route::post('/generate', GenerateController::class)->name('generate');
